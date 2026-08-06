2b8b31e9b7ae7d920d6e49f93af1568b881b9966
yujueq <f231242625@gmail.com>
2026-08-06 13:59:28 +0800

    Update PlayerController.js
    添加移動指令
    新增斜向移動的速度修正
    完成移動動畫

--- src/game/core/PlayerController.js
+++ src/game/core/PlayerController.js
@@ -1,32 +1,72 @@
 // 核心系統：主角（大象＋騎象人）在大地圖上的移動控制，使用 WASD／方向鍵
 export class PlayerController {
 
-    constructor(scene, sprite, speed = 200) {
+    constructor(scene, x, y) {
         this.scene = scene;
-        this.sprite = sprite;
-        this.speed = speed;
+        //建立主角(預設靜止圖樣)
+        this.sprite = scene.physics.add.sprite(x, y, 'main_character_stand_still');
+        this.sprite.setCollideWorldBounds(true);
+        
+        this.speed = 160;
+        //輸入鍵盤監聽
         this.cursors = scene.input.keyboard.createCursorKeys();
-        this.keys = scene.input.keyboard.addKeys('W,A,S,D');
+        this.wasd = scene.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D
+        });
+
+        this.initAnimations();
     }
 
+    initAnimations(){
+        //移動動畫
+        if(!this.scene.anims.exists('walk')){
+            this.scene.anims.create({
+                key: 'walk',
+                frames:[
+                    {key: 'main_character_moving_01'},
+                    {key: 'main_character_moving_01'}
+                ],
+                frameRate: 6,
+                repeat: -1
+            });
+        }
+    }
     // 每個 frame 呼叫一次，依按鍵狀態更新主角速度與朝向
     update() {
         const body = this.sprite.body;
         if (!body) return;
 
-        let vx = 0;
-        let vy = 0;
+        let velocityX = 0;
+        let velocityY = 0;
 
-        if (this.cursors.left.isDown || this.keys.A.isDown) vx -= 1;
-        if (this.cursors.right.isDown || this.keys.D.isDown) vx += 1;
-        if (this.cursors.up.isDown || this.keys.W.isDown) vy -= 1;
-        if (this.cursors.down.isDown || this.keys.S.isDown) vy += 1;
-
-        const length = Math.hypot(vx, vy) || 1;
-        body.setVelocity((vx / length) * this.speed, (vy / length) * this.speed);
-
-        if (vx !== 0) {
-            this.sprite.setFlipX(vx < 0);
+        if (this.cursors.left.isDown || this.wasd.left.isDown) {
+            velocityX -= this.speed;
+            this.sprite.setFlipX(true);//向左翻轉
         }
-    }
+        if (this.cursors.right.isDown || this.wasd.right.isDown) {
+            velocityX += this.speed;
+            this.sprite.setFlipX(false);
+        }
+        if (this.cursors.up.isDown || this.wasd.up.isDown) velocityY -= this.speed;
+        if (this.cursors.down.isDown || this.wasd.down.isDown) velocityY += this.speed;
+
+        // 對角線斜向移動時進行速度等比修正
+        if (velocityX !== 0 && velocityY !== 0) {
+            velocityX *= 0.7071;
+            velocityY *= 0.7071;
+        }
+        this.sprite.setVelocity(velocityX, velocityY);
+        
+        // 切換動畫或靜止狀態
+        if (velocityX !== 0 || velocityY !== 0) {
+          this.sprite.play('walk', true);
+        } else {
+          this.sprite.stop();
+          this.sprite.setTexture('main_character_stand_still');
+        }
+  }
 }
+
