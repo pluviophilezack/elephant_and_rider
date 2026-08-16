96c3091c7abdbb0e6c4128da3279c6f065bf66b6
YOU <amwayzack@gmail.com>
2026-08-16 13:08:44 +0800

    Add main character controller and its sprite. Add pickupRegistry.

--- src/game/scenes/Overworld.js
+++ src/game/scenes/Overworld.js
@@ -13,15 +13,16 @@ import authorityHerd from '../events/authority_herd';
 import domainElephants from '../events/domain_elephants';
 
 // 將 Phaser 掛載到全域，修正其他模組中「Phaser is not defined」的錯誤
+// Note：The code out of export... only run one time when this module loaded.
 window.Phaser = Phaser;
-
 const { Scene } = Phaser;
 
 export class Overworld extends Scene
 {
     constructor ()
     {
-        super('Overworld');
+        
+        super('Overworld'); // Note: Run constructor in parent class(Phaser.Scene)
     }
 
     create ()
@@ -30,6 +31,17 @@ export class Overworld extends Scene
         MoralState.reset();
 
         this.hud = new HUD(this);
+
+        // 建立主角Controller
+        this.playerController = new PlayerController(this, 400, 300);
+        this.wandController = new WandController(this, this.playerController);
+        // 供事件模組（如 domain_elephants）進行物理碰撞/重疊偵測
+        this.player = this.playerController.sprite; 
+
+        // TODO: 建立相機模組，傳入this.player
+
+        // 建立拾取物紀錄區，方便各事件呼叫
+        this.pickupRegistry = createPickupRegistry(this, this.wandController);
 
         // 各事件自行建立 sprite／觸發區域／按鍵監聽／可拾取物註冊，並在條件成立時自己呼叫 onEnter(this)
         this.events_ = [tutorial, ingroupBirdContest, fairnessWater, purityFloodedRuins, authorityHerd, domainElephants];
@@ -45,6 +57,9 @@ export class Overworld extends Scene
     }
 
     update () {
+        if (this.playerController) this.playerController.update();
+        if (this.wandController) this.wandController.update();
+
         this.events_.forEach(event => event.update(this));
     }
 }
